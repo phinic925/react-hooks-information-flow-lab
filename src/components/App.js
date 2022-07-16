d60d3a38ec29cc60ffe9ad2e89af6f27217a18b6
phinic925 <phinicnyangate480@gmail.com>
2022-07-16 14:22:24 +0100

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -13,6 +14,11 @@ function App() {
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
+        <Header isDarkMode={isDarkMode}onDarkModeClick={handleDarkModeClick}/>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+
         <button onClick={handleDarkModeClick}>
           {isDarkMode ? "Dark" : "Light"} Mode
         </button>
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header({isDarkMode, onDarkModeClick}){
+    return(
+        <Header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>{isDarkMode? "Dark":"Light"} Mode</button>
+        </Header>
+    )
+
+}
+export default Header;
